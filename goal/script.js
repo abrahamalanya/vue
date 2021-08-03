d812dc4f0d62cf9eeba8370bb92939e66b628a8f
Abraham Alanya <alanyaoficial@gmail.com>
2021-08-03 10:13:49 -0500

    goals by abraham

---
+++ goal/script.js
@@ -0,0 +1,45 @@
+var goal = new Vue({
+    el: '#goal',
+    data: {
+        month: 'August 📅',
+        title: 'Goal One ⚽',
+        content: [
+            {
+                description: 'Comprar un saco de comida para los perros.'
+            },
+            {
+                description: 'Matricularme en el gym 💪. x 1 mes nada mas jaja.'
+            },
+            {
+                description: 'Realizar un viaje, por 1 día al menos. (Destino - Aleatorio / Fuera de Huancayo)'
+            }
+        ]
+    }
+});
+var goal1 = new Vue({
+    el: '#goal-1',
+    data: {
+        month: 'September 📅',
+        title: 'Goal two ⚽',
+        content: [
+            {
+                description: 'Comprar un libro de Ibero o Crisol. (cualquier género)'
+            },
+            {
+                description: 'Ahorrar 200 para la cama nueva :V'
+            }
+        ]
+    }
+});
+var goal2 = new Vue({
+    el: '#goal-2',
+    data: {
+        month: 'October 📅',
+        title: 'Goal Three ⚽',
+        content: [
+            {
+                description: 'Ahorrar 200 para la cama nueva :V'
+            }
+        ]
+    }
+});
